7c7ff220961762cd2939c6dfa388e39116b26c8f
Duc Nguyen <duc@carousellmotors.com>
2018-01-24 23:26:01 +0700

    Init

---
+++ packages/react-web/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Hello} from '@example/components'
+import {View, Text} from 'react-native'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>test</p>
+        <View>
+          <Text>hello222</Text>
+        </View>
+      </div>
+    );
+  }
+}
+
+export default App;
